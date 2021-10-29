8a20dedfcbf63a89b7e757eaab4a0bbd9b83672b
Sanni Lateef Anjola <sanni.anjola@gmail.com>
2021-10-29 11:08:36 +0100

    Updated update service mock test

--- src/test/java/com/ecommerce/shop/service/ProductServiceImplMockTest.java
+++ src/test/java/com/ecommerce/shop/service/ProductServiceImplMockTest.java
@@ -5,6 +5,7 @@ import com.ecommerce.shop.data.model.Product;
 import com.ecommerce.shop.data.repository.ProductRepository;
 import com.ecommerce.shop.service.mapper.ProductMapper;
 import com.ecommerce.shop.web.exception.ProductDoesNotExistException;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.*;
@@ -15,6 +16,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+@Slf4j
 class ProductServiceImplMockTest {
 
     @Mock
@@ -48,8 +50,10 @@ class ProductServiceImplMockTest {
         product.setPrice(500.0);
         ProductDto productDto = new ProductDto();
         productDto.setPrice(1200.0);
+        productDto.setName("John Doe");
         when(productMapper.mapDtoToProduct(productDto, product)).then(e -> {
             product.setPrice(productDto.getPrice());
+            product.setName(productDto.getName());
             return null;
         });
         when(productRepository.findById(1L)).thenReturn(Optional.of(product));
@@ -61,5 +65,6 @@ class ProductServiceImplMockTest {
         verify(productRepository, times(1)).findById(1L);
         verify(productRepository, times(1)).save(product);
         assertThat(product.getPrice()).isEqualTo(1200.0);
+        assertThat(product.getName()).isEqualTo("John Doe");
     }
 }
